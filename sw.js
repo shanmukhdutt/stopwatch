f3f6af18c4bbe90c8299b75a8a88fff589834382
shanmukhdutt <ksssaidutt@gmail.com>
2023-08-25 12:05:05 +0530

    added a stopwatch file

---
+++ sw.js
@@ -0,0 +1,37 @@
+let timerDisplay=document.querySelector('.timerDisplay');
+let stopBtn=document.getElementById('stopBtn');
+let resetBtn=document.getElementById('resetBtn');
+let msec=00;
+let sec=00;
+let min=00;
+let timerId= null;
+startBtn.addEventListener('click',function(){
+    if(timerId!==null){
+        clearInterval(timerId);
+    }
+    timerId=setInterval(startTimer,10);
+})
+stopBtn.addEventListener('click',function(){
+    clearInterval(timerId);
+});
+resetBtn.addEventListener('click',function(){
+    clearInterval(timerId);
+    timerDisplay.innerHTML=`00:00:00`;
+    msec=sec=min=00;
+});
+function startTimer(){
+    msec++;
+   if(msec==100){
+    msec=0;
+    sec++;
+    if(sec==60){
+        sec=0;
+        min++;
+    }
+   }
+   let msecString = msec < 10 ? `0${msec}`:msec;
+   let secsString = sec < 10 ? `0${sec}`:sec;
+   let minsString = min < 10 ? `0${min}`:min;
+   timerDisplay.innerHTML =  `${msecString}:${secsString}:${minsString}`
+}
+
